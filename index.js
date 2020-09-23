9cd4c2053a00f03de09dac18f3df5d1b446ab68f
aaudit <rahulkumarhacker@gmail.com>
2020-09-23 23:20:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+// var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomImage = "images/dice"+ randomNumber1 +".png";
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImage); 
+
+
+
+// var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomImage = "images/dice"+ randomNumber2 +".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImage);
+
+if (randomNumber1 > randomNumber2) {
+	document.querySelector("h1").innerHTML = "Player1 Wins!";
+} else if(randomNumber1 < randomNumber2){
+	document.querySelector("h1").innerHTML = "Player2 Wins!";
+} else {
+	document.querySelector("h1").innerHTML = "Draw!";
+}
